86f223651ec20be2cc0497d74568e0cea9f1f391
rodrigoadelinoarxi <rodrigo.adelino@arxi.pt>
2026-07-16 09:15:32 +0100

    Add files via upload

---
+++ arxi-quality/arxi_quality_instance_client/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name'       : "Arxi Quality Instance Client",
+    'sequence'   : 42,
+    'license'    : 'OPL-1',
+    'summary'    : """Endpoint to communicate with Arxi Quality Tests for AI functionalities""",
+
+    'author'     : "ARXILEAD",
+    'website'    : "https://www.arxi.pt",
+
+    'category'   : 'Manufacturing/Quality',
+    'version'    : '15.0.0.0.10',
+
+    'depends'    : [
+    ],
+    'data'       : [
+    ],
+
+    'installable': True,
+}
