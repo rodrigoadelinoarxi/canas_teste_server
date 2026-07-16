e9f79444daee74cd23872930623591fa69cc0b15
rodrigoadelinoarxi <rodrigo.adelino@arxi.pt>
2026-07-16 09:13:06 +0100

    Delete canas/models directory

--- canas/models/hr_timesheet.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
-
-    repair_id = fields.Many2one('repair.order')
--- canas/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    fax = fields.Char(help="Fax Contact")
-    supplier_code = fields.Char(string="Supplier Code")
