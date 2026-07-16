8b2cc78594c6902598f01be99861745d4c3d518d
rodrigoadelinoarxi <rodrigo.adelino@arxi.pt>
2026-07-16 09:13:43 +0100

    Delete l10n_mz_hr_payroll directory

--- l10n_mz_hr_payroll/models/hr_employee.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
-
-    subject_to_social_security = fields.Boolean("Subject to Social Security?", default=True)
