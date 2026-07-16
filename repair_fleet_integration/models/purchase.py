4e3ed283aad00e2762d9b47f2b45dd7e9265585a
rodrigoadelinoarxi <rodrigo.adelino@arxi.pt>
2026-07-16 09:14:21 +0100

    Delete repair_fleet_integration/models directory

--- repair_fleet_integration/models/purchase.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class PruchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    repair_id = fields.Many2one('repair.order', 'Repair')
-    project_task_id = fields.Many2one('project.task', 'Work Number')
